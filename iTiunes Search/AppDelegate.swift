0d178d74fa875d883c2087c4c19f0edee00dd283
George Navarro <george@navarro.cc>
2019-02-19 18:36:22 -0800

    Initial Commit

---
+++ iTiunes Search/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  iTiunes Search
+//
+//  Created by George Navarro on 2/19/19.
+//  Copyright © 2019 Seventh Bit Studios. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain class AppDelegate: UIResponder, UIApplicationDelegate {
+	var window: UIWindow?
+}
